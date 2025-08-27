1002e23064b4bfda7ec91455fa435e4936696dbf
devanshu-fitterfly <107918529+fitterfly-devanshu@users.noreply.github.com>
2025-08-27 13:17:28 +0530

    fix: url path strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:surveyor_app/my_app.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
   await GetStorage.init();
   runApp(const MyApp());
 }
